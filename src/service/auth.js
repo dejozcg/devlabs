c6602efad5f04dadcc792c55e28e41d2182aa42d
Dejan Zarkovic <dejozcg@yahoo.com>
2023-09-14 10:21:20 +0200

    dan 3

--- src/service/auth.js
+++
@@ -1,7 +0,0 @@
-import { nodeBackend } from './api';
-
-class AuthService {
-  getLoggedInUser = async data => await nodeBackend.post('/login', data);
-}
-
-export const authService = new AuthService();
